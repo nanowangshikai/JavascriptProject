46ed7e8c6d5a07b32486948b055e06ee51686518
nano <henrywangkaikai@gmail.com>
2019-05-20 17:34:36 +0800

    Deleted extra folders

--- CITS3403Project 2/main.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from app.models import User, Brand, Choice, Record
-
-
-@app.shell_context_processor
-
-def make_shell_context():
-    return {'db' : db, 'User' : User, 'Brand' : Brand, 'Choice' : Choice, 'Record' : Record}
-
